ebc7e0103ce207e85b3c634504c86ee188c7c075
Lakshmithiru5 <rajalakshmithiru5@gmail.com>
2026-03-25 10:24:16 +0530

    25-03-2026 First commit

--- src/test/java/steps/loginSteps.java
+++ src/test/java/steps/loginSteps.java
@@ -9,30 +9,45 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class loginSteps {
+	    @Given("user is on login page")
+	    public void openLoginPage() {
+	        System.out.println("Opening banking login page");
+	    }
 
-	WebDriver driver;
-	@Given("Navigating to the webpage")
-	public void navigatingToTheWebpage() {
-		driver = new ChromeDriver();
-		driver.get("https://www.amazon.in/");
-	    
+	    @When("user enters username and password")
+	    public void enterCredentials() {
+	        System.out.println("Entering credentials");
+	    }
+
+	    @Then("user should see dashboard")
+	    public void verifyDashboard() {
+	        System.out.println("Dashboard displayed");
+	    }
 	}
-//	@Given("Enter item name")
-//	public void enterItemName() {
-//	  driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("Slipper");
+
+//	WebDriver driver;
+//	@Given("Navigating to the webpage")
+//	public void navigatingToTheWebpage() {
+//		driver = new ChromeDriver();
+//		driver.get("https://www.amazon.in/");
+//	    
 //	}
-	
-	@Given("Enter item name as {string}")
-	public void enterItemNameAs(String itemName) {
-		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys(itemName);
-	}
-	@When("Click Search")
-	public void clickSearch() {
-		 driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
-	}
-	@Then("Show Results")
-	public void showResults() {
-	   driver.close();
-	}
+////	@Given("Enter item name")
+////	public void enterItemName() {
+////	  driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("Slipper");
+////	}
+//	
+//	@Given("Enter item name as {string}")
+//	public void enterItemNameAs(String itemName) {
+//		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys(itemName);
+//	}
+//	@When("Click Search")
+//	public void clickSearch() {
+//		 driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
+//	}
+//	@Then("Show Results")
+//	public void showResults() {
+//	   driver.close();
+//	}
 
-}
+//}
--- target/test-classes/fetaures/login.feature
+++ target/test-classes/fetaures/login.feature
@@ -1,15 +1,22 @@
-Feature: Login Functionality
+# Feature: Login Functionality
 
-Scenario Outline: Performing Login Functionality
+#Scenario Outline: Performing Login Functionality
 
-Given Navigating to the webpage
-And Enter item name as "<items>"
-When Click Search
-Then Show Results
+# Given Navigating to the webpage
+# And Enter item name as "<items>"
+ # When Click Search
+# Then Show Results
 
-Examples:
-|	username	|	password	|
-|	mobile		|	bgcbg		|
-|	tv			|	jhgvkjhgv	|
-|	books		|	jhygfvjhv	|
+# Examples:
+# |	username	|	password	|
+# |	mobile		|	bgcbg		|
+# |	tv			|	jhgvkjhgv	|
+# |	books		|	jhygfvjhv	| 
+
+Feature: Banking Login
+
+  Scenario: Valid login
+    Given user is on login page
+    When user enters username and password
+    Then user should see dashboard
 
